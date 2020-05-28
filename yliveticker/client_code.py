435d8f5bce1524c638f32c8f9d21b33b33339977
alparamonov <alex.s.paramonov@gmail.com>
2020-05-29 01:44:01 +0200

    correct call to module in client code

--- yliveticker/client_code.py
+++ yliveticker/client_code.py
@@ -11,7 +11,7 @@ def on_close():
 
 print("Test output:", yliveticker.yliveticker.test())
 
-yliveticker.yliveticker(on_ticker=printRes,
+yliveticker.YLiveTicker(on_ticker=printRes,
                                   on_close=on_close,
                                   ticker_names=[
                                       "BTC=X", "^GSPC", "^DJI", "^IXIC", "^RUT", "CL=F", "GC=F", "SI=F", "EURUSD=X", "^TNX", "^VIX", "GBPUSD=X", "JPY=X", "BTC-USD", "^CMC200", "^FTSE", "^N225"])
