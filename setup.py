6c23bab1df59a2906b14ff50aedad3bacba02e41
Alexey Paramonov <alex.s.paramonov@gmail.com>
2020-10-25 00:35:44 +0200

    Increments the package version for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yliveticker",
-    version="0.2.6",
+    version="0.2.7",
     author="Alexey Paramonov",
     author_email="yliveticker@gmail.com",
     description="Live market data from Yahoo! Finance websocket",
