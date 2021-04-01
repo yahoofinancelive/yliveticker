ce73f5fd1daac807d5b9a6ec358e1c7e16620b83
alparamonov <alex.s.paramonov@gmail.com>
2021-04-01 20:09:09 +0200

    Version update for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yliveticker",
-    version="0.2.8",
+    version="0.2.9",
     author="Alexey Paramonov",
     author_email="yliveticker@gmail.com",
     description="Live market data from Yahoo! Finance websocket",
