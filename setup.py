1ab0649c08d6e96108c3e298f76cf23be1b19a67
alparamonov <alex.s.paramonov@gmail.com>
2026-03-25 00:26:35 +0100

    Update author and copyright name to Alex Paramonov

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="yliveticker",
     version="0.3.0",
-    author="Alexey Paramonov",
+    author="Alex Paramonov",
     author_email="yliveticker@gmail.com",
     description="Live market data from Yahoo! Finance websocket",
     long_description=long_description,
