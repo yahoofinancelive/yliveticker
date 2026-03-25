e52df2c273631930f7d95e4b37421a0472d455d2
alparamonov <alex.s.paramonov@gmail.com>
2026-03-25 14:29:03 +0100

    Bump version to 0.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yliveticker",
-    version="0.5.2",
+    version="0.5.3",
     author="Alex Paramonov",
     author_email="yliveticker@gmail.com",
     description="Live market data from Yahoo! Finance websocket",
